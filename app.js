56715b3e9faeff6591220c4dd1f71100402bed3f
Pokeyy123 <attilafodor603@gmail.com>
2022-11-12 11:48:17 +0200

    Made 16x16 grid

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const container = document.createElement('div')
 container.classList.add('container');
 document.body.appendChild(container);
 container.style.color= "black";  
-container.style.width = "100px";
-container.style.height = "100px";
+container.style.width = "272px";
+container.style.height = "272px";
 container.style.border = "1px solid";
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.alignContent = "flex-start";
+
+for(i = 0; i < 256; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    container.appendChild(grid);
+    grid.style.border = "1px solid grey" 
+    grid.style.width = "15px";
+    grid.style.height = "15px";
+    grid.style.color = "yellow";
+
+}
 
 
-const grid = document.createElement('div');
-grid.classList.add('grid');
-grid.style.backgroundColor = "yellow"; 
-grid.style.width = "16px";
-grid.style.height = "16px";
-grid.style.color = "yellow";
-
-
-document.querySelector('.grid').appendChild(container);
